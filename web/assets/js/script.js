b6caf7df7ca0eddf1c5f659fd512224e52b4fd6e
Ing. Jonathan Olier <djom202@gmail.com>
2015-04-26 03:48:48 -0500

    Change style home section

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -1,3 +1,24 @@
-$(function(){
+$( document ).ready(function() {
 	'use-strict';
+
+	setTimeout(function(){
+		inicio();
+	}, 300);
+
+
+	$(window).on('resize', function(){
+	     inicio();
+	});
 });
+
+function inicio(){
+	/* INICIO */
+	var height = $(window).height();
+	$(".logo").css("height", height);
+	$(".logotipo").css("padding-top", "10%");
+
+	/* CATEGORIAS */
+	height = parseInt($("#home>footer>div").css("width"));
+	height -= 20;
+	$(".colorClass").css("height", height+"px");
+}
